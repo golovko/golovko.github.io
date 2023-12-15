e5038bb21ea56d3dfe05bf127b488e1d34484cd0
Sergii Golovko <golovko.sergey@gmail.com>
2023-12-15 10:10:33 +0000

    fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,4 @@
-import React from 'react';
-
-type Props = {};
-
-export default function Header(props: Props) {
+export default function Header() {
   return (
     <section className="bg-white dark:bg-gray-900 bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] dark:bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern-dark.svg')]">
       <div className='py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 z-10 relative'>
